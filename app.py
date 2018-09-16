1c936873cd2ce2aba995419832ef1ae26b9b3015
Arjun Nair <arjun90.nair@gmail.com>
2018-09-16 15:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template,request, redirect,url_for
 from flask_mysqldb import MySQL
 
 import json
+
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_DB'] = 'matrimony'
